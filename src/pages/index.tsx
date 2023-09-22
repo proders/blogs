33ea4a6ca4d33bb76baedce39e9305406e738069
PGD <1925511879@qq.com>
2023-09-22 16:52:33 +0800

    improve: 添加图像

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,56 +13,56 @@ import ReactSlick from "@site/src/components/ReactSlick";
 import Text from "@site/src/components/text";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+    const {siteConfig} = useDocusaurusContext();
 
-  return (
-      <div className={styles.heroBanner}>
-          <div className={styles.leftSvg} >
-              <img src='img/svg/color-scheme-left.svg' />
-          </div>
+    return (
+        <div className={styles.heroBanner}>
+            <div className={styles.leftSvg}>
+                <img src='img/svg/color-scheme-left.svg'/>
+            </div>
 
-          <div className="container">
-              <h1 className="hero__title">{siteConfig.title}</h1>
-              <p className="hero__subtitle">{siteConfig.tagline}</p>
-              <div className={styles.buttons}>
-                  <Link
-                      className="button button--secondary button--lg"
-                      to="/docs/life">
-                      <BorderLight >
-                          文档阅读 5min ⏱️
-                      </BorderLight>
-                  </Link>
-              </div>
-          </div>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/life">
+                        <BorderLight>
+                            文档阅读 5min ⏱️
+                        </BorderLight>
+                    </Link>
+                </div>
+            </div>
 
-          <div className={styles.rightSvg} >
-              <img src='img/svg/color-scheme-right.svg'/>
-          </div>
-      </div>
- /*   <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className={styles.rightSvg}>
+                <img src='img/svg/color-scheme-right.svg'/>
+            </div>
+        </div>
+        /*   <header className={clsx('hero hero--primary', styles.heroBanner)}>
 
-    </header>
-      */
-  );
+           </header>
+             */
+    );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-        <BorderLight >没有想法啊</BorderLight>
-          <BrowserWindow>测试哈哈哈哈</BrowserWindow>
-          <HighLight color='#25c2a0'>高亮当前文本</HighLight>
-          <Confetti />
-          <TypingEffect />
-          {/*<ReactSlick />*/}
-        {/*<Text>show Time333333333333 !</Text>*/}
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />">
+            <HomepageHeader/>
+            <main>
+                <HomepageFeatures/>
+                <BorderLight>没有想法啊</BorderLight>
+                <BrowserWindow>测试哈哈哈哈</BrowserWindow>
+                <HighLight color='#25c2a0'>高亮当前文本</HighLight>
+                <Confetti/>
+                <TypingEffect/>
+                {/*<ReactSlick />*/}
+                {/*<Text>show Time333333333333 !</Text>*/}
+            </main>
+        </Layout>
+    );
 }
